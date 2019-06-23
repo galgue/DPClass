030e159984f02006f98963320150fa2502fbc8c9
galgue <galgue11@gmail.com>
2019-06-23 16:00:07 +0300

    Chain2.0

--- ChainOfResp/src/BillHandler.java
+++ ChainOfResp/src/BillHandler.java
@@ -2,9 +2,10 @@ public abstract class BillHandler {
 
     protected BillHandler next;
 
-    public void setNext(BillHandler next)
+    public BillHandler setNext(BillHandler next)
     {
         this.next = next;
+        return next;
     }
 
     public abstract void handle(int amount);
--- ChainOfResp/src/project.java
+++ ChainOfResp/src/project.java
@@ -1,16 +1,15 @@
 public class project {
     public static void main(String[] args) {
+        Bill b500 = new Bill(500);
         Bill b200 = new Bill(200);
         Bill b100 = new Bill(100);
-        b200.setNext(b100);
         Bill b50 = new Bill(50);
-        b100.setNext(b50);
         Bill b20 = new Bill(20);
-        b50.setNext(b20);
         Bill b5 = new Bill(5);
-        b20.setNext(b5);
 
-        b200.handle(588);
+        b500.setNext(b200).setNext(b100).setNext(b50).setNext(b20).setNext(b5);
+
+        b500.handle(62416161);
 
     }
 }
